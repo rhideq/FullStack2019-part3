e9a9e48ba5c16d6734d290cfb36be9c7243a310a
hiski <hiski.r@gmail.com>
2019-04-03 15:00:31 +0300

    pro1

---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const app = express();
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body_on_post'))
+
+morgan.token('body_on_post', (req, res) => {
+  return req.method === "POST" ? JSON.stringify(req.body) : null
+})
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "045-1236543"
+  },
+  {
+    id: 2,
+    name: "Arto Järvinen",
+    number: "041-21423123"
+  },
+  {
+    id: 3,
+    name: "Lea Kutvonen",
+    number: "040-4323234"
+  },
+  {
+    id: 4,
+    name: "Martti Tienari",
+    number: "09-784232"
+  }
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const newId = Math.floor(Math.random() * 9999)
+  return newId + 1;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "give name and number"
+    });
+  }
+
+  if (persons.map(person => person.name).includes(body.name)) {
+    return response.status(400).json({
+      error: "name must be unique"
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+app.get("/info", (request, response) => {
+  response.send(
+    `Puhelinluettelossa ${persons.length} henkilön tiedot <br> ${new Date()}`
+  );
+});
+
+const port = 3001;
+app.listen(port);
+console.log(`Server running on port ${port}`);
