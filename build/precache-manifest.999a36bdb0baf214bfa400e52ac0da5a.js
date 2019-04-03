fea0f24b06a4acecccd252da766e058d3b2ebcdd
hiski <hiski.r@gmail.com>
2019-04-03 15:13:28 +0300

    pro2

---
+++ build/precache-manifest.999a36bdb0baf214bfa400e52ac0da5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3ea93135469ce7f37add",
+    "url": "/static/js/main.d3e74580.chunk.js"
+  },
+  {
+    "revision": "86e5e33a435a04249221",
+    "url": "/static/js/2.1dc66a3a.chunk.js"
+  },
+  {
+    "revision": "3ea93135469ce7f37add",
+    "url": "/static/css/main.3d3bf004.chunk.css"
+  },
+  {
+    "revision": "6bb0e9bdb0425df386fb100e9c55530e",
+    "url": "/index.html"
+  }
+];
